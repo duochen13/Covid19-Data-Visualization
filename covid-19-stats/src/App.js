58eac686a2fc3c1888804eecfb102a54c0740ad1
duochen13 <duochen@umich.edu>
2020-03-30 05:20:28 -0400

    add files

---
+++ covid-19-stats/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Covid 19 Stats</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
