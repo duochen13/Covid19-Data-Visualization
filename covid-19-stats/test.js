6574e7bd5c6eefbd012ee9e32b29286e72980d8c
duochen13 <duochen@umich.edu>
2020-03-30 20:02:39 -0400

    add files

--- covid-19-stats/test.js
+++
@@ -1,8 +0,0 @@
-let confirmed = [{x:"1", y:"a"}, {x:"2", y:"b"}]
-//let res = confirmed.map((data) =>  {key:data.x, value:data.y})
-
-var res = confirmed.map(function(val, index){ 
-            return {[val.x] : val.y}; 
-        }) 
-
-console.log(res)
