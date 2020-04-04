be34fcda89b786a46da70273ef01ca45a5804cd9
duochen13 <duochen@umich.edu>
2020-04-04 03:37:20 -0400

    fix layout

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import './App.css';
 import { LineChart } from 'react-chartkick'
 import 'chart.js'
 import  MyMapComponent from './Map.js'
+import SearchBar from './SearchBar.js'
 // import { google } from 'google-maps';
 
 // const google=window.google
@@ -27,6 +28,7 @@ class App extends React.Component {
       dataList: [] //
     }
     this.changeCountry = this.changeCountry.bind(this)
+    this.clickReset = this.clickReset.bind(this)
   }
   changeCountry(item) {
     this.setState({
@@ -40,6 +42,13 @@ class App extends React.Component {
         countryList: [...prevState.countryList, item]
       }))
     }
+  }
+  clickReset() {
+    this.setState({
+      headerTitle: "Click me to select a country",
+      countryList: [],
+      dataList: []
+    })
   }
   fetchConfirmedCases(country) {
     // const country = this.state.headerTitle
@@ -88,103 +97,24 @@ class App extends React.Component {
     
     return (
       <div className="App">
-        {/* <div className="App-graph">
-          <LineChart data={this.state.dataList} />
-        </div> */}
        
         <MyMapComponent />
- 
-  
-        <LineChart data={this.state.dataList} />
-        <div className="App-searchbar">
-          <p><strong>{this.state.headerTitle}</strong> Covid-19 Confirmed Cases ~ Date</p>
+
+        <div id="App-searchbar">
           <SearchBar list={this.state.country_names}
             headerTitle={this.state.headerTitle}
-            changeCountry={this.changeCountry}/>
-          <button onClick={() => this.setState({
-            headerTitle: "Click me to select a country",
-            countryList: [],
-            dataList: []
-          })}>Reset</button>
-          {/* <CountryList countryList={this.state.countryList} /> */}
+            changeCountry={this.changeCountry}
+            clickReset={this.clickReset} />
         </div>
-        <div className="App-body">
-         
-            {/* <LineChart data={{"2011235-13": 2, "2017-05-14": 5}} /> */}
-            {/* <LineChart data={this.state.data} />
-          */}
-            {/* <MyMapComponent /> */}
 
+        <div id="App-chart">
+          <LineChart data={this.state.dataList} />
         </div>
+
       </div>
     );
   }
 }
 
-// props: list, headerTitle, changeCountry
-class SearchBar extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      listOpen: false,
-      // headerTitle: this.props.headerTitle
-    }
-  }
-  handleClickOutside(){
-    this.setState({
-      listOpen: false
-    })
-  }
-  toggleList(){
-    this.setState(prevState => ({
-      listOpen: !prevState.listOpen
-    }))
-  }
-  changeCountry(item) {
-    this.props.changeCountry(item)
-    this.setState({
-      // headerTitle: item,
-      listOpen: false
-    })
-  }
-  render() {
-    // const{list, headerTitle} = this.props
-    const {list} = this.props
-    const{listOpen} = this.state
-    
-    return (
-    <div>
-      <div className="dd-wrapper">
-        <div className="dd-header" onClick={() => this.toggleList()}>
-              <div className="dd-header-title">Click to select countries</div>
-        </div>
-        {listOpen && <ul className="dd-list">
-            {list.map((item, index) => (
-              <li className="dd-list-item" key={index} 
-                onClick={() => this.changeCountry(item)}>{item}</li>
-              ))}
-            </ul>}
-      </div>
-    </div>
-    )
-  }
-}
-
-// class Map extends React.Component {
-//   render() {
-//     const MyMapComponent = (props) =>
-//       <GoogleMap
-//         defaultZoom={8}
-//         defaultCenter={{ lat: -34.397, lng: 150.644 }}
-//       >
-//         {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-//       </GoogleMap>
-//     return (
-//       <div>
-//         <MyMapComponent isMarkerShown={false} />// Just only Map
-//       </div>
-//     )
-//   }
-// }
 
 export default App;
---
+++ src/SearchBar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+// props: list, headerTitle, changeCountry, clickReset
+class SearchBar extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        listOpen: false,
+        // headerTitle: this.props.headerTitle
+      }
+    }
+    handleClickOutside(){
+      this.setState({
+        listOpen: false
+      })
+    }
+    toggleList(){
+      this.setState(prevState => ({
+        listOpen: !prevState.listOpen
+      }))
+    }
+    changeCountry(item) {
+      this.props.changeCountry(item)
+      this.setState({
+        // headerTitle: item,
+        listOpen: false
+      })
+    }
+    clickReset() {
+        this.props.clickReset()
+    }
+    render() {
+      // const{list, headerTitle} = this.props
+      const {list} = this.props
+      const{listOpen} = this.state
+      
+      return (
+        <div id="App-searchbar">
+            <div className="dd-wrapper">
+            <div className="dd-header" onClick={() => this.toggleList()}>
+                    <div className="dd-header-title">Click to select countries</div>
+            </div>
+            {listOpen && <ul className="dd-list">
+                {list.map((item, index) => (
+                    <li className="dd-list-item" key={index} 
+                    onClick={() => this.changeCountry(item)}>{item}</li>
+                    ))}
+                </ul>}
+            </div>
+            <button onClick={() => this.clickReset()} >Reset</button>
+        </div>
+      )
+    }
+  }
+
+export default SearchBar;
